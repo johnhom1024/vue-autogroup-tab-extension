f6a8ad516651df376ed748fb827e7fbc55b3ea64
anthony <924358746@qq.com>
2022-09-18 00:23:54 +0800

    first commit

---
+++ src/popup/components/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>{{ text }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const text = ref('hello world 111')
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,53 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const copyFiles = [
+  {
+    from: path.resolve('src/plugins/manifest.json'),
+    to: `${path.resolve('dist')}/manifest.json`,
+  },
+  {
+    from: path.resolve('src/assets'),
+    to: path.resolve('dist/assets'),
+  },
+];
+
+// 复制插件
+const plugins = [
+  new CopyWebpackPlugin({
+    patterns: copyFiles,
+  }),
+];
+
+const pages = {};
+const chromePageName = ['popup'];
+
+chromePageName.forEach((name) => {
+  pages[name] = {
+    entry: `src/${name}/main.ts`,
+    template: `src/${name}/index.html`,
+    filename: `${name}.html`,
+  };
+});
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  productionSourceMap: false,
+  pages,
+  configureWebpack: {
+    entry: {
+      background: './src/background/main.ts',
+    },
+    output: {
+      filename: 'js/[name].js',
+    },
+    plugins,
+  },
+  // 配置 content.css
+  css: {
+    extract: {
+      filename: 'css/[name].css',
+    },
+  },
+});
