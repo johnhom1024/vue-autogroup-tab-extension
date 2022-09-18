1a5f4526feda6cc03622f2659dd92dedef28f1d2
anthony <924358746@qq.com>
2022-09-18 22:53:09 +0800

    docs: 优化getDomain注视

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,19 @@
 
-// 获取url的域名
+/**
+ * @description: 获取url的域名
+ * @param {string} url
+ * @return {string｜ null} 返回url中的域名信息
+ * @example 
+ * 例如：
+ * 接收 https://linuxtools-rst.readthedocs.io:9090/zh_CN/latest/index.html
+ * 返回 linuxtools-rst.readthedocs.io
+ */
 export function getDomain(url: string | undefined | null) {
   if (!url) {
-    return '';
+    return null;
   }
   const re = /^https?:\/\/([^/:]+)(:\d+)?\/.*/;
   const match = url.match(re);
   return match ? match[1] : null;
 }
+
