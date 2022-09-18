4f86ff6dc6c04b6a59db043e4fdc9f4085f57861
anthony <924358746@qq.com>
2022-09-18 23:48:39 +0800

    feat: 引入antd-vue和tailwindcss

--- src/popup/components/App.vue
+++ src/popup/components/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <div class="popup-container">{{ text }}</div>
+  <div class="w-[300px] bg-[#f9fafb]">
+    <div class="p-[10px] flex items-center">
+      <div class="font-bold text-[22px]">标签自动分组</div>
+      <div class="flex-auto"></div>
+      <a-switch v-model:checked="autoGroupSwitch"></a-switch>
+    </div>
+    <a-divider style="margin: 0"></a-divider>
+    <div class="p-[10px]">
+      <!-- <a-form
+        layout="horizontal"
+        :labelCol="{ span: 4 }"
+        :wrapperCol="{ span: 14 }"
+      >
+        <a-form-item label="自动分组">
+        </a-form-item>
+      </a-form> -->
+      <a-button type="primary">一键分组</a-button>
+    </div>
+
+    <!-- <a-divider style="margin: 0"/> -->
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const text = ref('hello world 111')
+const autoGroupSwitch = ref(true);
 </script>
 
-
 <style lang="scss" scoped>
-.popup-container {
-  width: 500px;
-}
-  
+@import 'tailwindcss/utilities';
+@import 'tailwindcss/components';
 </style>
--- src/popup/main.ts
+++ src/popup/main.ts
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './components/App.vue'
+import { createApp } from 'vue';
+import App from './components/App.vue';
+// 引入antd-vue
+import { Button, Form, Switch, Divider } from 'ant-design-vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(Button).use(Form).use(Switch).use(Divider);
+
+app.mount('#app');
