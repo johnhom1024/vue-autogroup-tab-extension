473e8fec454eabec8d763408e2516f8d4ec882a3
anthony <924358746@qq.com>
2022-09-18 17:37:32 +0800

    feat: 支持sass预处理loader

--- src/popup/components/App.vue
+++ src/popup/components/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>{{ text }}</div>
+  <div class="popup-container">{{ text }}</div>
 </template>
 
 <script setup lang="ts">
@@ -7,3 +7,11 @@ import { ref } from 'vue';
 
 const text = ref('hello world 111')
 </script>
+
+
+<style lang="scss" scoped>
+.popup-container {
+  width: 500px;
+}
+  
+</style>
