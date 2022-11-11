0f9bd55313d6d121364d849f90c2db254bdbe9bf
anthony <924358746@qq.com>
2022-11-11 10:07:46 +0800

    feat: 引入@johnhom/eslint-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,8 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
   },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+  extends: ['@johnhom']
+};
